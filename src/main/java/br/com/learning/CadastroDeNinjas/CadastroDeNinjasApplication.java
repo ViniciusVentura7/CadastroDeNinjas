6fb7154ba2afd4b00004fa637ee176937fa99a53
vinicius <viniven7@gmail.com>
2026-01-21 21:00:36 -0300

    fix: CadastroDeNinjasApplication and NinjaModel classes fixed.

--- src/main/java/br/com/learning/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/br/com/learning/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -1,4 +1,4 @@
-package br.com.learning.CadastroDeNinjas.Ninjas;
+package br.com.learning.CadastroDeNinjas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
