305a3f5b2651c187f7ff681aa496268d7c510353
vinicius <viniven7@gmail.com>
2026-01-26 21:52:57 -0300

    feat: created the endpoints of MissoesController mocked.

--- src/main/java/br/com/learning/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/br/com/learning/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,15 +1,44 @@
 package br.com.learning.CadastroDeNinjas.Missoes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("missoes")
 public class MissoesController {
 
     @GetMapping("/tela_missoes_controller")
     public String telaMissoesController(){
         return "Tela de controller de missoes.";
     }
+
+    // Create
+    @PostMapping("/adicionar")
+    public String adicionarMissao(){
+        return "Missão adicionada";
+    }
+
+    // Read
+    @GetMapping("/listar")
+    public String mostrarMissoes(){
+        return "Mostrar missoes";
+    }
+
+    @GetMapping("/listarId")
+    public String mostrarMissoesId(){
+        return "Mostrar missoes por Id: XX...";
+    }
+
+
+    // Update
+    @PutMapping("/alterarId")
+    public String alterarMissoesId(){
+        return "Missão de Id: XXX, alterada!";
+    }
+
+    // Delete
+    @DeleteMapping("/deletarId")
+    public String deletarMissaoId(){
+        return "Missão de ID: XXX, deletada com sucesso!";
+    }
+
 }
