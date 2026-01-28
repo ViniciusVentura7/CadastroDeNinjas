7b97b36e15bfe11c0c5ea82115e0c98d0877c27f
vinicius <viniven7@gmail.com>
2026-01-27 21:23:46 -0300

    fix: MissoesServie updated with MissoesRepository implemention.

--- src/main/java/br/com/learning/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/br/com/learning/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,4 +1,28 @@
 package br.com.learning.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    // Adicionar missão
+
+
+    // Listar missões
+    public List<MissoesModel> listarMissoes(){
+        return missoesRepository.findAll();
+    }
+
+    // Atualizar missão
+
+
+    // Deletar missão
 }
