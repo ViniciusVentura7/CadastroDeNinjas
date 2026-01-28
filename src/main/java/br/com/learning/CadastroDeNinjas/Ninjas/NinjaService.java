8fd1fdb68007765429e25c9d6de9809b4507a790
vinicius <viniven7@gmail.com>
2026-01-27 21:19:32 -0300

    fix: NinjaService class updated.

--- src/main/java/br/com/learning/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/br/com/learning/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -1,4 +1,28 @@
 package br.com.learning.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class NinjaService {
+
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    // Adicionar ninja
+
+
+    // Listar ninja
+    public List<NinjaModel> listarNinjas(){
+        return ninjaRepository.findAll();
+    }
+
+    // Atualizar ninja
+
+    // Deletar ninja
+
 }
